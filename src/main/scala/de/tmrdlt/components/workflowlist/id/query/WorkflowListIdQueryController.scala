9b6b6be4b3eb1ecb7cf5cd7a1442b5197f5d775e
Timo Erdelt <ti.erdelt@gmail.com>
2021-07-18 17:36:56 +0200

    Support recursive structure

--- src/main/scala/de/tmrdlt/components/workflowlist/id/query/WorkflowListIdQueryController.scala
+++ src/main/scala/de/tmrdlt/components/workflowlist/id/query/WorkflowListIdQueryController.scala
@@ -1,7 +1,9 @@
 package de.tmrdlt.components.workflowlist.id.query
 
+import de.tmrdlt.components.workflowlist.id.query.WorkflowListColumnType.WorkflowListColumnType
 import de.tmrdlt.database.event.{Event, EventDB}
-import de.tmrdlt.models.{TemporalQueryResultEntity, WorkflowListEntity}
+import de.tmrdlt.models.WorkflowListType.WorkflowListType
+import de.tmrdlt.models.{TemporalQueryResultEntity, TemporalResourceEntity, WorkflowListEntity, WorkflowListType}
 import de.tmrdlt.services.WorkflowListService
 import de.tmrdlt.utils.SimpleNameLogger
 
@@ -12,6 +14,35 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.math.Ordered.orderingToOrdered
 
+// TODO move elsewhere
+object WorkflowListColumnType extends Enumeration {
+  type WorkflowListColumnType = Value
+  val OPEN, IN_PROGRESS = Value
+}
+
+case class WorkflowListTemporalQuery(apiId: String,
+                                     title: String,
+                                     temporalResource: Option[TemporalResourceEntity],
+                                     workflowListType: WorkflowListType,
+                                     columnType: WorkflowListColumnType,
+                                     predictedDuration: Long)
+
+case class ExecutionOrderWl(apiId: String,
+                            title: String,
+                            predictedDuration: Long,
+                           )
+
+case class WorkflowListExecutionTreeEvaluation(executionOrder: Seq[String],
+                                               calculatedEndDate: LocalDateTime,
+                                               numberOfDueDatesFailed: Int) // TODO To show in frontend make object, which contains wl ID and projected due date
+
+object WorkflowListExecutionTreeEvaluation {
+  // Note that because `Ordering[A]` is not contravariant, the declaration
+  // must be type-parametrized in the event that you want the implicit
+  // ordering to apply to subclasses of `Employee`.
+  implicit def ordering[A <: WorkflowListExecutionTreeEvaluation]: Ordering[A] =
+    Ordering.by(t => (t.numberOfDueDatesFailed, t.calculatedEndDate))
+}
 
 class WorkflowListIdQueryController(workflowListService: WorkflowListService,
                                     eventDB: EventDB) extends SimpleNameLogger {
@@ -21,68 +52,135 @@ class WorkflowListIdQueryController(workflowListService: WorkflowListService,
   val stopWorkAtHour: Int = 18
   val assumedDurationForTasksWithoutDuration: Int = 0
 
-  // TODO make recursive for all children
-  // Annahmen:
-  // 1. Erste Spalte des Boards entspricht open items, letzte Spalte entspricht done items, alle dazwischen sind in progress items
+
   def getDurationOfAllTasks(workflowListApiId: String): Future[TemporalQueryResultEntity] = {
     val now = LocalDateTime.now()
     for {
       board <- workflowListService.getWorkflowListEntityForId(workflowListApiId)
       events <- eventDB.getEvents
     } yield {
-      val open = board.children.head
-      val done = board.children.last.children
-      val inProgress = board.children.drop(1).dropRight(1)
+      if (board.children.size < 2) {
+        throw new Exception("Board doesnt have required columns")
+      } else {
+        // Defined as first column of board
+        val openColumn = board.children.head
+        // Defined as last column of board
+        val doneColumn = board.children.last.children
+        // Defined as columns between first and last column of board
+        val inProgressColumns = board.children.drop(1).dropRight(1)
 
-      val durationOfProject = getDurationOfWorkflowListsInOpen(open) + getDurationOfWorkflowListsInProgress(open, inProgress, events)
-      val totalFinishDateByDuration = getFinishDateForStartDateAndDuration(now, durationOfProject)
-      val bestExecutionResult = getBestExecutionOrderOfTasks(now, open.children)
+        val allWorkflowListsFlattened = workflowListRecFlatten(
+          Seq(openColumn),
+          WorkflowListColumnType.OPEN,
+          openColumn.apiId,
+          inProgressColumns.map(_.apiId),
+          events,
+          now) ++
+          workflowListRecFlatten(inProgressColumns,
+            WorkflowListColumnType.IN_PROGRESS,
+            openColumn.apiId,
+            inProgressColumns.map(_.apiId),
+            events,
+            now)
 
+        val totalDuration = allWorkflowListsFlattened.map(_.predictedDuration).sum
 
-      TemporalQueryResultEntity(
-        totalDurationMinutes = durationOfProject,
-        totalFinishDateByDuration = totalFinishDateByDuration,
-        openTasksPredictedFinishDate = bestExecutionResult.calculatedEndDate,
-        bestExecutionOrder = bestExecutionResult.executionOrder,
-        numberOfFailedDueDates = bestExecutionResult.numberOfDueDatesFailed
-      )
+        val totalFinishDateByDuration = getFinishDateForStartDateAndDuration(now, totalDuration)
+        val bestExecutionResult = getBestExecutionOrderOfTasks(now, allWorkflowListsFlattened)
 
+        TemporalQueryResultEntity(
+          totalDurationMinutes = totalDuration,
+          totalFinishDateByDuration = totalFinishDateByDuration,
+          openTasksPredictedFinishDate = bestExecutionResult.calculatedEndDate,
+          bestExecutionOrder = bestExecutionResult.executionOrder,
+          numberOfFailedDueDates = bestExecutionResult.numberOfDueDatesFailed
+        )
+      }
     }
   }
 
-  def getDurationOfWorkflowListsInOpen(open: WorkflowListEntity): Long = {
-    // TODO take all tasks here. Tasks without duration: Either duration is assumed 0 or duration, one place to get this
-    open.children.flatMap(_.temporalResource.flatMap(_.durationInMinutes)).sum
+  private def workflowListRecFlatten(workflowLists: Seq[WorkflowListEntity],
+                                     columnType: WorkflowListColumnType,
+                                     openApiId: String,
+                                     inProgressApiIds: Seq[String],
+                                     events: Seq[Event],
+                                     now: LocalDateTime): Seq[WorkflowListTemporalQuery] = {
+    workflowLists.map(wl =>
+      WorkflowListTemporalQuery(
+        wl.apiId,
+        wl.title,
+        wl.temporalResource,
+        wl.usageType, columnType,
+        getPredictedDurationOfWorkflowList(workflowList = wl, columnType, openApiId, inProgressApiIds, events, now)
+      )
+    ) ++
+      workflowLists.flatMap(wl =>
+        workflowListRecFlatten(
+          wl.children,
+          columnType,
+          openApiId,
+          inProgressApiIds,
+          events,
+          now)
+      )
   }
 
-  def getDurationOfWorkflowListsInProgress(open: WorkflowListEntity, inProgress: Seq[WorkflowListEntity], events: Seq[Event]): Long = {
-    // D_total = SUM(D_inProgress - T_inProgress)
-    // TODO take all tasks here. Tasks without duration: Either duration is assumed 0 or duration, one place to get this
-    val allItemsInProgressWithDurations = inProgress.flatMap(_.children).filter(item => item.temporalResource.isDefined && item.temporalResource.get.durationInMinutes.isDefined)
+  private def getPredictedDurationOfWorkflowList(workflowList: WorkflowListEntity,
+                                                 columnType: WorkflowListColumnType,
+                                                 openApiId: String,
+                                                 inProgressApiIds: Seq[String],
+                                                 events: Seq[Event],
+                                                 now: LocalDateTime): Long = {
 
-    allItemsInProgressWithDurations.map { itemInProgress =>
-      val movedToInProgressDateOption = events.filter(e => e.workflowListApiId == itemInProgress.apiId && e.oldParentApiId.contains(open.apiId)).sortBy(_.date).headOption.map(_.date)
-      val createdAtInProgressDateOption = events.filter(e => e.workflowListApiId == itemInProgress.apiId && e.parentApiId.contains(open.apiId)).sortBy(_.date).headOption.map(_.date)
-      val now = LocalDateTime.now()
+    def getMinutesInProgress(workflowListApiId: String,
+                             openApiId: String,
+                             inProgressApiIds: Seq[String],
+                             events: Seq[Event],
+                             now: LocalDateTime): Long = {
+      // TODO this doesnt work for nested children as they might not be moved directly into in progress
+      val movedToInProgressDateOption = events
+        .filter(e => e.workflowListApiId == workflowListApiId &&
+          e.oldParentApiId.contains(openApiId) &&
+          inProgressApiIds.contains(e.newParentApiId.getOrElse("")))
+        .sortBy(_.date)
+        .headOption
+        .map(_.date)
+
+      val createdAtInProgressDateOption = events
+        .filter(e => e.workflowListApiId == workflowListApiId &&
+          e.parentApiId.contains(openApiId) &&
+          inProgressApiIds.contains(e.newParentApiId.getOrElse("")))
+        .sortBy(_.date)
+        .headOption
+        .map(_.date)
 
       // Time in inProgress: Time passed since moved to inProgress OR time passed sinced created in inProgress
-      val timeInProgress = (movedToInProgressDateOption, createdAtInProgressDateOption) match {
+      (movedToInProgressDateOption, createdAtInProgressDateOption) match {
         case (Some(fromDate), _) => ChronoUnit.MINUTES.between(fromDate, now)
         case (_, Some(fromDate)) => ChronoUnit.MINUTES.between(fromDate, now)
         case _ => 0
       }
+    }
 
-      // When longer in progress than duration: Task is finished, predicted duration = 0
-      val durationFromNow = itemInProgress.temporalResource.get.durationInMinutes.get - timeInProgress
-      if (durationFromNow > 0) {
-        durationFromNow
-      } else {
-        0
-      }
-    }.sum
+    columnType match {
+      // Predicted duration = estimated duration
+      case WorkflowListColumnType.OPEN => workflowList.temporalResource.flatMap(_.durationInMinutes).getOrElse(0)
+      // Predicted duration = estimated duration - time in progress
+      case WorkflowListColumnType.IN_PROGRESS =>
+        val durationFromNow = workflowList.temporalResource.flatMap(_.durationInMinutes) match {
+          case Some(duration) => duration - getMinutesInProgress(workflowList.apiId, openApiId, inProgressApiIds, events, now)
+          case None => 0
+        }
+        // When longer in progress than estimated duration: Task should be finished, predicted duration = 0
+        if (durationFromNow > 0) {
+          durationFromNow
+        } else {
+          0
+        }
+    }
   }
 
-  def getFinishDateForStartDateAndDuration(startDate: LocalDateTime, durationInMinutes: Long): LocalDateTime = {
+  private def getFinishDateForStartDateAndDuration(startDate: LocalDateTime, durationInMinutes: Long): LocalDateTime = {
 
     @tailrec
     def getFinishDateRecursive(startDate: LocalDateTime, durationInMinutes: Long): LocalDateTime = {
@@ -142,41 +240,26 @@ class WorkflowListIdQueryController(workflowListService: WorkflowListService,
   }
 
 
-  def getBestExecutionOrderOfTasks(now: LocalDateTime, openTasks: Seq[WorkflowListEntity]): WorkflowListExecutionTreeEvaluation = {
-    // Only schedule tasks with a duration defined TODO take all tasks here. Tasks without duration: Either duration is assumed 0 or duration, one place to get this
-    val relevantTasks = openTasks.filter(item => item.temporalResource.isDefined && item.temporalResource.get.durationInMinutes.isDefined)
-
-    relevantTasks.permutations.map { tasksPermutation =>
+  private def getBestExecutionOrderOfTasks(now: LocalDateTime, workflowLists: Seq[WorkflowListTemporalQuery]): WorkflowListExecutionTreeEvaluation = {
+    workflowLists.filter(_.workflowListType == WorkflowListType.ITEM).permutations.map { tasksPermutation =>
       var startDate = now
       var endDate = now
       var numberOfDueDatesFailed = 0
       // TODO make recursive function
-      tasksPermutation.foreach { task =>
-        startDate = task.getStartDate match {
-          case Some(date) => Seq(date, startDate).max
-          case _ => endDate
+      tasksPermutation
+        .foreach { workflowList =>
+          startDate = workflowList.temporalResource.flatMap(_.startDate) match {
+            case Some(date) => Seq(date, startDate).max
+            case _ => endDate
+          }
+          endDate = getFinishDateForStartDateAndDuration(startDate, workflowList.predictedDuration)
+          //log.info("endDate" + endDate)
+          if (workflowList.temporalResource.flatMap(_.endDate).exists(_ < endDate)) {
+            numberOfDueDatesFailed = numberOfDueDatesFailed + 1
+          }
         }
-        endDate = getFinishDateForStartDateAndDuration(startDate, task.temporalResource.get.durationInMinutes.get)
-        //log.info("endDate" + endDate)
-        if (task.getEndDate.exists(_ < endDate)) {
-          numberOfDueDatesFailed = numberOfDueDatesFailed + 1
-        }
-      }
       WorkflowListExecutionTreeEvaluation(tasksPermutation.map(_.title), endDate, numberOfDueDatesFailed)
     }.toSeq.min
   }
-}
-
-// TODO move elsewhere
-case class WorkflowListExecutionTreeEvaluation(executionOrder: Seq[String],
-                                               calculatedEndDate: LocalDateTime,
-                                               numberOfDueDatesFailed: Int) // TODO To show in frontend make object, which contains wl ID and projected due date
-
-object WorkflowListExecutionTreeEvaluation {
-  // Note that because `Ordering[A]` is not contravariant, the declaration
-  // must be type-parametrized in the event that you want the implicit
-  // ordering to apply to subclasses of `Employee`.
-  implicit def ordering[A <: WorkflowListExecutionTreeEvaluation]: Ordering[A] =
-    Ordering.by(t => (t.numberOfDueDatesFailed, t.calculatedEndDate))
 }
 
